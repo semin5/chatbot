773502ed0ce51f3a99cd53df08b2dd55a85b51b4
semin5 <omhmir1@gamil.com>
2026-02-27 04:44:40 +0900

    ai chatbot

--- src/main/java/com/rocket/chatbot/dto/ChatRequest.java
+++ src/main/java/com/rocket/chatbot/dto/ChatRequest.java
@@ -1,11 +1,13 @@
 package com.rocket.chatbot.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class ChatRequest {
+    @JsonIgnore
     private Long conversationId;
     private String message;
 }
